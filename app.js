30afceba504f82d2974f290bf23808fbc9fc09f3
loconluis <luis.locon@gmail.com>
2016-11-17 22:53:26 -0600

    se agrego el controlador y el servidor de socket

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//Creacion del servidor con express
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-//Model de la peticion
+//Modelo de la peticion
 var Request = require('./models/request').Request;
 
 //Uso de archivos estaticos
@@ -18,9 +19,31 @@ app.get('/', function(req, res) {
 });
 
 //Servidor de sockets
-io.on('connection', function(){
+io.sockets.on('connection', function(){
     console.log('Usuario conectado');
-    
+
+
+    //Envio de Mensaje
+    socket.on('req-send', function(username, email, req){
+        socket.emit('req-push', req, username);
+        socket.broadcast.emit('req-ṕush', req, username);
+        //Se agrega a la base de Datos
+        var peticion = new Request();
+        peticion.author = username;
+        peticion.message = req;
+        peticion.email = email;
+        peticion.date = new Date();
+        peticion.save(function(err){
+                if(err){console.log(err);}
+        })
+    });
+
+    //Se desconecto usuario
+    socket.on('disconnect', function(){
+        console.log('Se desconecto usario');
+    })
+
+
 });
 
 
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,2 +1,24 @@
 //Archivo de manejo en JavaScript
+
+//Punto de conexion con Sockets
 var socket = io.connect('http://localhost:3000', {forceNew: true});
+
+socket.on('connect', function() {
+    socket.emit('req-update');
+});
+
+socket.on('req-push', function(req, username){
+    
+});
+
+socket.on('req-open', function(){
+    //Recorrido
+});
+
+function sendRequest(){
+    var username = document.getElementById('userame').value;
+    var email = document.getElementById('email').value;
+    var  req = document.getElementById('request').value;
+
+    socket.emit('req-send', username, email, req);
+};
